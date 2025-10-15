11a0ecc71a960460539810e04f14c091ee7f2682
carterethan <carterethan180@gmail.com>
2025-10-15 16:52:10 +0800

    gm record date

--- src/WheelxGM.sol
+++ src/WheelxGM.sol
@@ -7,6 +7,11 @@ contract WheelXGMContract {
     address public immutable recipient;
     uint256 public immutable minimalFee;
 
+    mapping(address => uint256) public dates;
+
+    error TransferFailed(bytes data);
+    error AlreadyGmToday();
+
     constructor(address _recipient, uint256 _minimalFee) {
         recipient = _recipient;
         minimalFee = _minimalFee;
@@ -15,10 +20,18 @@ contract WheelXGMContract {
     function gm() public payable {
         require(msg.value >= minimalFee, "Insufficient fee");
 
-        (bool success, ) = recipient.call{value: msg.value}("");
-        require(success, "Fee transfer failed");
+        // convert timestamp to date
+        uint date = block.timestamp / 86400;
+        if (dates[msg.sender] == date) {
+            revert AlreadyGmToday();
+        }
+        dates[msg.sender] = date;
+
+        (bool success, bytes memory data) = recipient.call{value: msg.value}("");
+        if (!success) {
+            revert TransferFailed(data);
+        }
 
         emit WheelXGM(msg.sender);
     }
 }
-
