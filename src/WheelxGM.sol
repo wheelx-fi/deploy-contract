93a8a0fe409c2de45b95c4b807e5597abb54da9b
carterethan <carterethan180@gmail.com>
2025-11-20 14:12:48 +0800

    no limit in gm

--- src/WheelxGM.sol
+++ src/WheelxGM.sol
@@ -7,10 +7,7 @@ contract WheelXGMContract {
     address public immutable recipient;
     uint256 public immutable minimalFee;
 
-    mapping(address => uint256) public dates;
-
     error TransferFailed(bytes data);
-    error AlreadyGmToday();
 
     constructor(address _recipient, uint256 _minimalFee) {
         recipient = _recipient;
@@ -19,13 +16,6 @@ contract WheelXGMContract {
 
     function gm() public payable {
         require(msg.value >= minimalFee, "Insufficient fee");
-
-        // convert timestamp to date
-        uint date = block.timestamp / 86400;
-        if (dates[msg.sender] == date) {
-            revert AlreadyGmToday();
-        }
-        dates[msg.sender] = date;
 
         (bool success, bytes memory data) = recipient.call{value: msg.value}("");
         if (!success) {
